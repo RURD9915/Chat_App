79960a00b628d0e87ea1ed145d83926bdfe21e24
RURD9915 <rurd9915@gmail.com>
2025-01-25 15:20:58 +0530

    Chat_app

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*ojv(+h_%b)&gk-47=uo^(90+!z*2-(q)$f=#m2o0wg$-^2ke9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
