cb3d7ae1ba9daf7103526b127d602c69578028d6
RURD9915 <rurd9915@gmail.com>
2025-01-25 17:09:43 +0530

    settings

--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['chat-app-uy07.onrender.com']
+ALLOWED_HOSTS = ['chat-app-uy07.onrender.com','localhost']
 
 # Application definition
 
