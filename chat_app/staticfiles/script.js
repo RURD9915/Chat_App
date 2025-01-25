832eda5a99d011e09e90c4e487f0b1a72c4af1e1
RURD9915 <rurd9915@gmail.com>
2025-01-25 18:19:40 +0530

    js

--- chat_app/staticfiles/script.js
+++ chat_app/staticfiles/script.js
@@ -35,18 +35,19 @@ window.addEventListener("resize", adjustPageWidth);
 adjustPageWidth();
 
 // WebSocket Connection
-// WebSocket Connection
 let ws_scheme = window.location.protocol === "https:" ? "wss://" : "ws://";
 let ws_path =
   ws_scheme +
   window.location.host +
   window.location.pathname.replace("/chat/", "/ws/chat/");
-console.log("WebSocket URL: ", ws_path); // Log the WebSocket URL for verification
+console.log("Current Protocol: ", window.location.protocol); // Log current protocol
+console.log("WebSocket Scheme: ", ws_scheme); // Log WebSocket scheme
+console.log("WebSocket URL: ", ws_path); // Log the WebSocket URL
 let chatSocket = new WebSocket(ws_path);
 
 chatSocket.onmessage = function (e) {
   let data = JSON.parse(e.data);
-  console.log(data);
+  console.log("Message received: ", data); // Log received messages
 };
 
 chatSocket.onclose = function (e) {
